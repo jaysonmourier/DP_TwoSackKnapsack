fb2f43dfda9a1c6aff2be5680e0835f2d31e3542
Jayson Mourier <mourierjayson@protonmail.com>
2023-04-04 14:21:49 +0200

    added double_knapsack.c

--- double_knapsack.c
+++ double_knapsack.c
@@ -1,68 +1,122 @@
-/*
-    Clément De Miribel
-    Jayson Mourier
-*/
-
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-#define P 10
-#define Q 20
-#define N 8
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-int resolve(int *state, object *obj);
-int max(int a, int b, int c);
+#define N 5
+#define M 5
+#define Z 3
 
 typedef struct {
-    int w;
-    int p;
-} object;
+    int n;
+    int m;
+    int z;
+    int *value;
+} Cube;
 
-static object sacA[P];
-static object sacB[Q];
+Cube *init(int n, int m, int z);
+void set(Cube *c, int value, int i, int j, int k);
+int get(Cube *c, int i, int j, int k);
+void print_3D(Cube *c);
+
+Cube *max_value_two_sacks(int *values, int *weights, int n, int capacity1, int capacity2, int *result);
 
 int
 main(void)
 {
-    int state[2] = {P, Q};
-    object objects[N] = {
-        {3, 15},
-        {5, 30},
-        {1, 12},
-        {4, 20},
-        {2, 15},
-        {8, 34},
-        {12, 25},
-        {7, 43},
-    };
+    int values[] = {60, 100, 120, 70, 80};
+    int weights[] = {10, 20, 30, 40, 50};
+    int n = sizeof(values) / sizeof(values[0]);
+    int capacity1 = 10;
+    int capacity2 = 20;
 
-    for(int k = 0; k < N; ++k) resolve(state, objects);
+    int result = 0;
+    
+    Cube *c = max_value_two_sacks(values, weights, n, capacity1, capacity2, &result);
+
+    print_3D(c);    
+
+    printf("La valeur maximale que l'on peut transporter dans les deux sacs est : %d\n", result);
 
     return 0;
 }
 
-int resolve(int *state, object *obj)
+Cube *init(int n, int m, int z)
 {
-    int val = 0;
-    
-    if((state[0] >= obj->p) && (state[1] >= obj->p))
+    Cube *tmp = (Cube *) malloc(1 * sizeof(Cube));
+
+    if(tmp == NULL)
     {
-        val += max(obj->w + resolve(state[0] - obj->p, obj), obj->w + resolve(state[0], state[1] - obj->p), resolve(state, obj));
+        fprintf(stderr, "Erreur: impossible d'allouer la mémoire demandée.\n");
+        exit(1);
     }
 
-    return 0;
+    tmp->value = (int *) calloc(n * m * z, sizeof(Cube));
+
+    if(tmp->value == NULL)
+    {
+        fprintf(stderr, "Erreur: impossible d'allouer la mémoire demandée.\n");
+        exit(1);
+    }
+
+    tmp->n = n;
+    tmp->m = m;
+    tmp->z = z;
+
+    return tmp;
 }
 
-int max(int a, int b, int c) {
-   int max_val = a;
-   
-   if (b > max_val) {
-      max_val = b;
-   }
-   
-   if (c > max_val) {
-      max_val = c;
-   }
-   
-   return max_val;
+void set(Cube *c, int value, int i, int j, int k)
+{
+    *(c->value + j + i * c->m + k * c->n * c->m) = value;
+}
+
+int get(Cube *c, int i, int j, int k)
+{
+    return *(c->value + j + i * c->m + k * c->n * c->m);
+}
+
+void print_3D(Cube *c)
+{
+    int n = c->n;
+    int m = c->m;
+    int z = c->z;
+
+    for(int k = 0; k < z; ++k)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j < m; ++j)
+            {
+                fprintf(stdout, "%3d ", *(c->value + j + i * m + k * n * m));
+            }
+            putchar('\n');
+        }
+        putchar('\n');
+    }
+}
+
+Cube *max_value_two_sacks(int *values, int *weights, int n, int capacity1, int capacity2, int *result)
+{
+    Cube *dp = init(n + 1, capacity1 + 1, capacity2 + 1);
+    
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 0; j <= capacity1; ++j)
+        {
+            for (int k = 0; k <= capacity2; ++k)
+            {
+                int value_without_item = get(dp, i - 1, j, k);
+                int value_with_item_in_sack1 = (j >= weights[i - 1]) ? get(dp, i - 1, j - weights[i - 1], k) + values[i - 1] : 0;
+                int value_with_item_in_sack2 = (k >= weights[i - 1]) ? get(dp, i - 1, j, k - weights[i - 1]) + values[i - 1] : 0;
+                
+                int max_value = MAX(MAX(value_without_item, value_with_item_in_sack1), value_with_item_in_sack2);
+                set(dp, max_value, i, j, k);
+            }
+        }
+    }
+    
+    *result = get(dp, n, capacity1, capacity2);
+
+    return dp;
 }
